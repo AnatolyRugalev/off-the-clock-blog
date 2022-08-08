5c0933b839328850fb8eb1620200a2b3962d5bda
Anatoly Rugalev <anatoly.rugalev@gmail.com>
2022-08-08 21:43:36 +0200

    Add PanelBear tracking script

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,9 +1,27 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/ssr-apis/
- */
+import React from "react";
 
-exports.onRenderBody = ({ setHtmlAttributes }) => {
-  setHtmlAttributes({ lang: `en` })
-}
+const panelbearConfig = {
+  site: "484zFQ9lsHm",
+  spaMode: "history",
+  debug: false,
+  honorDNT: true,
+};
+
+export const onRenderBody = ({ setPostBodyComponents }) => {
+  setPostBodyComponents([
+    <script
+        key="panelbear-analytics-src"
+        async
+        src={`https://cdn.panelbear.com/analytics.js?site=${panelbearConfig.site}`}
+    />,
+    <script
+        key="panelbear-analytics-code"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.panelbear = window.panelbear || function() { (window.panelbear.q = window.panelbear.q || []).push(arguments); };
+          panelbear('config', ${JSON.stringify(panelbearConfig)});
+        `,
+        }}
+    />,
+  ]);
+};
